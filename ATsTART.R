4d2f8d82fd624527c1b7a88342499339a1a40f7c
Carlos Crosetti <15020728+carloscrosetti@users.noreply.github.com>
2019-02-09 12:20:40 -0300

    Add files via upload

---
+++ ATsTART.R
@@ -0,0 +1,7 @@
+setwd("C:/Users/cvjh/My Documents")
+getwd()
+# "C:/Users/cvjh/My Documents"
+# install.packages("caret")
+# package into ‘C:/Users/cvjh/OneDrive - Chevron/My Documents/R/win-library/3.4’
+# install.packages("rattle")
+# install.packages("RGtk2")
